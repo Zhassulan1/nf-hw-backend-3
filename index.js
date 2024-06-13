6822f7914c46eaf7087f2aae0e720b9518f2e5a9
Zhassulan1 <zhkainazarov@gmail.com>
2024-06-13 23:07:41 +0500

    finished

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ const app = express()
 const URL = 'https://habr.com/ru/articles/'
 
 let parsedArticles = [];
+let fetchTime = new Date().toLocaleString();;
 app.get('/fetch', (req, res) => {
     fetchHabrArticles(URL).then(articles => {
+        fetchTime = new Date().toLocaleString();;
         parsedArticles = articles;
     });
     res.send("Fetching articles...")
 });
 
+
+app.get('/lastfetch', (req, res) => {
+    res.send("Last fetch time: " + fetchTime + "\n")
+});
+
 app.get('/articles', (req, res) => {
     res.send(parsedArticles);
 });
-    
+
+
+app.get("/", (req, res) => {
+    res.send("Health check")
+})
+
 const PORT = 3003;    
 app.listen(PORT, () => {
     console.log(`Server runs at http://localhost:${PORT}`);
